7e98f4c88dab181d9cadb519cd3c86a2c7d45475
Christian Fries <christian.fries123@gmail.com>
2016-01-30 10:37:20 +0100

    add project selection to creator script

--- createBuild.sh
+++ createBuild.sh
@@ -1,19 +1,55 @@
 #!/bin/bash
 
-if [[ $# != 1 ]]; then
-	echo "usage: createBuild <BUILD_DIR>"
-	exit -1
-fi
+usage()
+{
+cat << EOF
 
-BUILD_DIR=$1
+Usage: $0 [p:h] <BUILD_DIR>
+    BUILD_DIR   build directory folder
 
-TPL_CONF="meta-rose/conf"
+    optional options:
+        -h          show this help message
+        -p Project  valid projects are "rose" [default] and "villa"
+EOF
+
+exit -1
+}
+
+BUILD_DIR=
+
+CONF=rose
+TPL_CONF=
 
 REPO_DIR=$(dirname $(readlink -e $(pwd)/../poky/oe-init-build-env))
 
 if [[ ! -e ${REPO_DIR}/oe-init-build-env ]];then
 	echo "$0 must be called direct"
 	exit -1
+fi
+
+while getopts 'p:h' OPTION ; do
+  case "$OPTION" in
+    p)   CONF=$OPTARG;;
+    h)  usage;;
+    *)  echo "Invalid option: -$OPTARG" >&2; usage;;
+  esac
+done
+
+shift $((OPTIND - 1))
+if [[ $# != 1 ]]; then
+    echo "No build dir given or additional parameter"
+    usage
+fi
+
+BUILD_DIR=$1
+
+if [[ "${CONF,,}" == "rose" ]]; then
+    TPL_CONF="meta-rose/conf"
+elif [[ "${CONF,,}" == "villa" ]]; then
+    TPL_CONF="meta-villa/conf"
+else
+    echo "Unknown project ${CONF}"
+    exit -2;
 fi
 
 pushd ${REPO_DIR}  > /dev/null
